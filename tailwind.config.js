fe404406fc50b2a0224f6be2fd3b3df9094d36c7
0xNight <night9uy@gmail.com>
2021-12-28 05:16:01 +0530

    feat: filmstip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-	content: ["./**/*.{html,js}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      ...colors,
+      primary: "#5fb0df",
+      primaryBackground: "#252525",
+    },
+    extend: {},
+  },
+  plugins: [],
 };
